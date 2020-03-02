7ffcdc31c6cb9b1538259d27617b81a4df8d7585
hejiangda <hejiangda@163.com>
2020-03-02 19:47:45 +0800

    试用googletest ok

--- GoogleTestDemo/tst_demo.h
+++ GoogleTestDemo/tst_demo.h
@@ -3,7 +3,7 @@
 
 #include <gtest/gtest.h>
 #include <gmock/gmock-matchers.h>
-
+#include <QString>
 using namespace testing;
 
 TEST(demo, demo)
@@ -11,5 +11,10 @@ TEST(demo, demo)
     EXPECT_EQ(1, 1);
     ASSERT_THAT(0, Eq(0));
 }
+TEST(string,string)
+{
+    QString str="Hello";
 
+    ASSERT_EQ(str.toUpper(),QString("HELLO"));
+}
 #endif // TST_DEMO_H
